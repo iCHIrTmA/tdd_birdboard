7334f781a5fd9d8144777b5606c03fd1ffd053c1
Janroe <janroe@nabepero.co.jp>
2020-11-30 12:32:21 +0800

    made unit ProjectTest

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -12,11 +12,19 @@ class ProjectsController extends Controller
     	$projects = Project::all();
 
     	return view('projects.index', compact('projects'));
+    }
+
+    public function show(Project $project)
+    {
+    	return view('projects.show', compact('project'));
     }	
 
     public function store()
     {
-    	$attributes = request()->validate(['title' => 'required', 'description' => 'required']);
+    	$attributes = request()->validate([
+    		'title' => 'required', 
+    		'description' => 'required'
+    	]);
 
     	Project::create($attributes);
 
