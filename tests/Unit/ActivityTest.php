e8dd1e1d7fffcc7588e4b2fa6709cfa05dddfc93
Janroe <janroe@nabepero.co.jp>
2020-12-09 13:37:08 +0800

    6:30 added user-activity relationship

---
+++ tests/Unit/ActivityTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Project;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ActivityTest extends TestCase
+{
+	use RefreshDatabase;
+
+	public function test_it_has_a_user()
+	{
+		$project = factory(Project::class)->create();
+
+		$this->assertInstanceOf(User::class, $project->activity->first()->user);
+	}
+}
