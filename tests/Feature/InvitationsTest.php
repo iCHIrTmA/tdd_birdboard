8227d7627c42448f15160cb4ddd5fd18f29d71a9
Janroe <janroe@nabepero.co.jp>
2020-12-11 09:46:31 +0800

    10:45

--- tests/Feature/InvitationsTest.php
+++ tests/Feature/InvitationsTest.php
@@ -12,16 +12,51 @@ class InvitationsTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_a_project_can_invite_a_user()
+    public function test_non_owners_may_not_invite_users()
+    {    	
+    	$this->actingAs(factory(User::class)->create())
+    		->post(ProjectTestFactory::create()->path() . '/invitations')
+    		->assertStatus(403);
+    }
+
+    public function test_a_project_owner_can_invite_a_user()
+    {	
+    	$project = ProjectTestFactory::create();
+
+    	$userToInvite = factory(User::class)->create();
+
+    	$this->actingAs($project->owner)
+    		->post($project->path() . '/invitations', [
+    			'email' => $userToInvite->email
+    		])
+    		->assertRedirect($project->path());
+
+    	$this->assertTrue($project->members->contains($userToInvite));
+    }
+
+    public function test_the_email_address_must_be_associated_with_a_valid_birdboard_account()
     {
-    
-    $project = ProjectTestFactory::create();
+    	$project = ProjectTestFactory::create();
 
-    $project->invite($newUser = factory(User::class)->create());
+    	$this->actingAs($project->owner)
+    		->post($project->path() . '/invitations', [
+    			'email' => 'notauser@example.com',
+    		])
+    		->assertSessionHasErrors([
+    			'email' => 'The user you are inviting must have a Birdboard account.',
+    		]);
+    }
 
-    $this->signIn($newUser);
-    $this->post(action('ProjectTasksController@store', $project), $task = ['body' => 'Foo task']);
-        
-    $this->assertDatabaseHas('tasks', $task);       
+    public function test_invited_users_may_update_project_details()
+    {    
+	    $project = ProjectTestFactory::create();
+
+	    $project->invite($newUser = factory(User::class)->create());
+
+	    $this
+	    	->actingAs($newUser)
+	    	->post(action('ProjectTasksController@store', $project), $task = ['body' => 'Foo task']);
+	        
+	    $this->assertDatabaseHas('tasks', $task);       
     }
 }
